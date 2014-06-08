7e83539b24dd1b00b3db5c594cc72d54873be418
icarus4 <icarus4.chu@gmail.com>
2014-06-08 17:39:13 +0800

    [M] modify Car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,12 @@
 class Car < ActiveRecord::Base
+
+  belongs_to :car_brands
+
+  validates :car_brand_id, presence: true
+  validates :model, presence: true
+  validates :submodel, presence: true
+  validates :made_in, presence: true, inclusion: { in: %w(Taiwan Japan U.S. India) }
+  validates :year, presence: true, numericality: { greater_than_or_equal_to: 2000 }
+  validates_uniqueness_of :submodel, scope: [:model, :made_in, :year]
+
 end
