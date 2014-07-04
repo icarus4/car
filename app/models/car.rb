cdf6a203fc1b464188d6df4e5bec7120ace939b9
icarus4 <icarus4.chu@gmail.com>
2014-07-04 20:44:50 +0800

    1. validate retail price. 2. validate uniqueness of submodel by more rules

--- app/models/car.rb
+++ app/models/car.rb
@@ -5,9 +5,10 @@ class Car < ActiveRecord::Base
   validates :brand_id, presence: true
   validates :model, presence: true
   validates :submodel, presence: true
+  validates :displacement, presence: true
   validates :made_in, presence: true, inclusion: { in: %w(Taiwan Japan U.S. India) }
   validates :year, presence: true, numericality: { greater_than_or_equal_to: 2000 }
-  validates_uniqueness_of :submodel, scope: [:model, :made_in, :year]
-  validates :price, numericality: { greater_than: 0 }
+  validates_uniqueness_of :submodel, scope: [:model, :made_in, :year, :displacement]
+  validates :retail_price, numericality: { greater_than: 0 }
 
 end
