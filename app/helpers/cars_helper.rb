4d7d12913e782e0644796a305e55cb09021b3485
icarus4 <icarus4.chu@gmail.com>
2014-07-23 11:35:24 +0800

    Code refine

--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -1,2 +1,5 @@
 module CarsHelper
+  def render_full_car_name(brand, car)
+    link_to("#{car.model} #{car.displacement} #{car.submodel}", { controller: :cars, action: :show, id: car.id }, class: 'side-nav-car-link')
+  end
 end
