bc0fdeb7ae5fd4e1ad42eebd4d19fa10396e0b38
icarus4 <icarus4.chu@gmail.com>
2014-07-23 19:54:24 +0800

    refine indent

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -17,20 +17,28 @@ class Brand < ActiveRecord::Base
   has_many :models
 
   validates :name,  presence: true, uniqueness: true,
-                    inclusion: { in: %w(BMW
-                                        Ford
-                                        Honda Hyundai
-                                        Infiniti
-                                        Lexus
-                                        M-Benz Mazda Mitsubishi
-                                        Nissan
-                                        Peugeot
-                                        Skoda Subaru Suzuki
-                                        Tobe Toyota
-                                        Volkswagen Volvo) }
+                    inclusion: {
+                      in: %w(
+                        BMW
+                        Ford
+                        Honda Hyundai
+                        Infiniti
+                        Lexus
+                        M-Benz Mazda Mitsubishi
+                        Nissan
+                        Peugeot
+                        Skoda Subaru Suzuki
+                        Tobe Toyota
+                        Volkswagen Volvo
+                      )
+                    }
 
   validates :headquarters,  allow_nil: true,
-                            inclusion: { in: %w(Japan
-                                                Taiwan
-                                                U.S.) }
+                            inclusion: {
+                              in: %w(
+                                Japan
+                                Taiwan
+                                U.S.
+                              )
+                            }
 end
