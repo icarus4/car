8052f91d334cd7c9bf0e9fe817b5f96395b73644
icarus4 <icarus4.chu@gmail.com>
2014-07-26 18:31:10 +0800

    1. font refine 2. remove unused function

--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -1,5 +1,2 @@
 module CarsHelper
-  def render_full_car_name(brand, car)
-    link_to("#{car.model} #{car.displacement} #{car.submodel}", { controller: :cars, action: :show, id: car.id }, class: 'side-nav-car-link')
-  end
 end
