bf623d7d3bf21af61d3f62b15cc93d356820ef4e
icarus4 <icarus4.chu@gmail.com>
2014-07-30 00:02:53 +0800

    show warning message if no car is found

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,6 +9,7 @@ class SearchController < ApplicationController
     @cars = Car.scoped
     @cars = @cars.where('airbag_num >= ?', params[:min_airbag_num]) if [0,2,4,6].include?(params[:min_airbag_num])
     @cars = @cars.where(has_airbags_at_side_curtain: true) if params[:has_airbags_at_side_curtain] == '1'
+    @cars = @cars.where(has_pretension_seat_belt: true) if params[:has_pretension_seat_belt] == '1'
     @cars = @cars.where(has_isofix: true) if params[:has_isofix] == '1'
     @cars = @cars.where(has_abs: true) if params[:has_abs] == '1'
     @cars = @cars.where(has_ebd: true) if params[:has_ebd] == '1'
@@ -16,5 +17,15 @@ class SearchController < ApplicationController
     @cars = @cars.where(has_esp: true) if params[:has_esp] == '1'
     @cars = @cars.where(has_cruise_control: true) if params[:has_cruise_control] == '1'
     @cars = @cars.where(has_blind_spot_monitor_system: true) if params[:has_blind_spot_monitor_system] == '1'
+    @cars = @cars.where(has_tpms: true) if params[:has_tpms] == '1'
+    @cars = @cars.where(has_afs: true) if params[:has_afs] == '1'
+
+    if @cars.empty?
+      flash[:warning] = '沒有符合條件的車子哦！請減少一些條件試試看'
+      render 'search/index/'
+    else
+      flash[:success] = '符合條件的車子如下：'
+      @cars
+    end
   end
 end
