e4f301d3d97596439ff8ce0fbbce30625433780f
icarus4 <icarus4.chu@gmail.com>
2014-08-02 10:08:34 +0800

    1. [BF] search condition of airbag number doesn't work 2. Limit search results to 60 cars 3. Show number of search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,7 +8,7 @@ class SearchController < ApplicationController
   def search
     # Todo: refactor search
     _cars = Car.scoped
-    _cars = _cars.where('airbag_num >= ?', params[:min_airbag_num]) if [0,2,4,6].include?(params[:min_airbag_num])
+    _cars = _cars.where('airbag_num >= ?', params[:min_airbag_num]) if ['0','2','4','6'].include?(params[:min_airbag_num])
     _cars = _cars.where(has_airbags_at_side_curtain: true) if params[:has_airbags_at_side_curtain] == '1'
     _cars = _cars.where(has_pretension_seat_belt: true) if params[:has_pretension_seat_belt] == '1'
     _cars = _cars.where(has_isofix: true) if params[:has_isofix] == '1'
@@ -22,13 +22,14 @@ class SearchController < ApplicationController
     _cars = _cars.where(has_afs: true) if params[:has_afs] == '1'
     _cars = _cars.where('retail_price <= ?', params[:max_price].to_i*10000) if params[:max_price].to_i > 0
 
-    _cars = _cars.order(:retail_price)
+    # Ordered by price, max result: 60
+    _cars = _cars.order(:retail_price).limit(60)
 
     if _cars.empty?
       flash[:warning] = '沒有符合條件的車子哦！請減少一些條件再試試看'
       redirect_to action: 'index'
     else
-      flash[:success] = '符合條件的車子如下（按照售價排序）：'
+      flash[:success] = "符合條件的車子共 #{_cars.size} 輛（按照售價排序）："
       @cars_group = _cars.each_slice(6).to_a
     end
   end
