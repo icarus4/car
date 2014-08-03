9b76ac0cd297f96e4d58f6964d004257bea6def7
icarus4 <icarus4.chu@gmail.com>
2014-08-03 22:11:05 +0800

    update validation of Brand

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -17,24 +17,7 @@ class Brand < ActiveRecord::Base
   has_many :models
   has_many :cars, through: :models
 
-  validates :name,  presence: true, uniqueness: true,
-    inclusion: {
-      in: %w(
-        BMW
-        Ford
-        Honda Hyundai
-        Infiniti
-        Lexus
-        M-Benz Mazda Mitsubishi
-        Nissan
-        Peugeot
-        Skoda Subaru Suzuki
-        Tobe Toyota
-        Volkswagen Volvo
-      )
-    }
-
-  validates :headquarters,  allow_nil: true
+  validates :name,  presence: true, uniqueness: true
 
   def headquarters_enum
     [['台灣'],['日本'],['美國'],['印度'],['德國'],['義大利'],['法國'],['中國'],['捷克']]
