2de193e6f097272ad127d0b817afe9aecc5b13e9
icarus4 <icarus4.chu@gmail.com>
2014-08-28 23:51:07 +0800

    rename welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SafetyCar::Application.routes.draw do
 
-  root "models#index"
+  root "welcomes#index"
+
   resources :models, only: [:index, :show]
 
   get 'search' => 'search#index'
@@ -25,7 +26,7 @@ SafetyCar::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root 'welcomes#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- spec/controllers/welcomes_controller_spec.rb
+++ spec/controllers/welcomes_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe WelcomeController do
+describe WelcomesController do
 
   describe "GET 'index'" do
     it "returns http success" do
