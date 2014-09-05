00edf4b3968385e1f25608e98b1ba414c9873242
icarus4 <icarus4.chu@gmail.com>
2014-09-05 12:44:24 +0800

    1. Redirect to previous url after login/logout 2. move omniauth settings from config/initializers/devise.rb to config/initializers/omniauth.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   layout :layout_by_resource
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :store_location
 
   protected
 
@@ -25,12 +26,27 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    return unless request.get?
+    if (request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        request.path != "/users/auth/facebook" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath
+    end
+  end
+
   def after_sign_in_path_for(resource)
-    root_path
+    session[:previous_url] || root_path
   end
 
   def after_sign_out_path_for(resource)
-    root_path
+    session[:previous_url] || root_path
   end
 
 end
