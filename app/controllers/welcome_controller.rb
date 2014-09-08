2f6594e6404249e0e25de5e89cdefd1a5cee3b40
icarus4 <icarus4.chu@gmail.com>
2014-09-08 17:32:26 +0800

    show new cars in landing page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @new_cars = Car.where("created_at >= :one_month_ago", :one_month_ago => Time.now - 30.days).order(created_at: :desc).limit(15)
   end
 end
