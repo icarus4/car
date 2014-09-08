80ab90b69e26c9d3f7f9e48671f3451a9f2200e3
icarus4 <icarus4.chu@gmail.com>
2014-09-08 16:34:16 +0800

    redirect to the previous page after login/logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # for redirect after sign_in/sign_out
+  after_action :store_location
+
+
   protected
 
   def configure_permitted_parameters
@@ -14,6 +18,21 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    return unless request.get?
+    if (request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        request.path != "/users/auth/facebook" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath
+    end
+  end
 
   def layout_by_resource
     if devise_controller?
@@ -26,11 +45,10 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(resource)
-    root_path
+    session[:previous_url] || root_path
   end
 
   def after_sign_out_path_for(resource)
-    root_path
+    session[:previous_url] || root_path
   end
-
 end
