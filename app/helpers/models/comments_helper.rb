073be343dc8dacd01d03fc8940fd9fb9484a6b1c
icarus4 <icarus4.chu@gmail.com>
2014-09-14 10:42:01 +0800

    Refine layout

--- app/helpers/models/comments_helper.rb
+++ app/helpers/models/comments_helper.rb
@@ -1,5 +1,5 @@
 module Models::CommentsHelper
-  def render_user_comment_preview(comment, max_character = 40)
+  def render_user_comment_preview(comment, max_character = 30)
     if comment.content.size > max_character
       "#{comment.content.first(max_character)} ..."
     else
