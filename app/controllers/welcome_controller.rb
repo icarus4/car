da62f9c72ca087bf80e6a324098c9a9a48ed5ea6
icarus4 <icarus4.chu@gmail.com>
2014-09-14 10:32:47 +0800

    1. show recently user comments 2. refine recently created cars

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @new_cars = Car.where("created_at >= :one_month_ago", :one_month_ago => Time.now - 30.days).order(created_at: :desc).limit(20)
+    @recently_created_cars = Car.where("created_at >= :one_month_ago", :one_month_ago => Time.now - 30.days).order(created_at: :desc).limit(20)
+    @recently_user_comments = Comment.recently_comments_on_model(last_n_days = 7, limit = 20)
   end
 end
